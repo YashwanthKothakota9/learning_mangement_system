0a23912f66faa39a9f7620205d56dc964b591d1c
YashwanthK <yashwanthkothakota@gmail.com>
2023-11-03 11:51:07 +0530

    Add sidebar

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,20 @@
+import { Logo } from "./logo";
+import { SidebarRoutes } from "./sidebar-routes";
+
+const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-y-auto bg-white shadow-sm">
+      <div className="p-6 flex gap-2 items-center justify-center">
+        <Logo />
+        <p className="text-2xl font-bold text-zinc-700 uppercase italic">
+          Coursely
+        </p>
+      </div>
+      <div className="flex flex-col w-full">
+        <SidebarRoutes />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
