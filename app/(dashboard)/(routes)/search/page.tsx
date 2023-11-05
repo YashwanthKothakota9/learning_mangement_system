6d5607836dec08f773af724c65c0f6889d920a46
YashwanthK <yashwanthkothakota@gmail.com>
2023-11-05 17:01:13 +0530

    Add categories query

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,18 @@
-const SearchPage = () => {
-  return <div>This is a search page</div>;
+import prisma from "@/lib/db";
+import { Categories } from "./_components/categories";
+
+const SearchPage = async () => {
+  const categories = await prisma.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+  );
 };
 
 export default SearchPage;
