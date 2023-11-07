1cc02094f4aeefdb55068d8637f04b4b0c6f02b2
YashwanthK <yashwanthkothakota@gmail.com>
2023-11-07 09:38:41 +0530

    Add chapter progress functionality

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { IconBadge } from "./icon-badge";
 import { BookOpen } from "lucide-react";
 import { formatPrice } from "@/lib/format";
+import { CourseProgress } from "./course-progress";
 
 interface CourseCardProps {
   id: string;
@@ -43,7 +44,11 @@ export const CourseCard = ({
             </div>
           </div>
           {progress !== null ? (
-            <div>Todo:Progress component</div>
+            <CourseProgress
+              value={progress}
+              size="sm"
+              variant={progress === 100 ? "success" : "default"}
+            />
           ) : (
             <p className="text-md md:text-xs font-medium text-slate-700">
               {formatPrice(price)}
